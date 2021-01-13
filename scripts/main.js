e81dac8b65ad69822c129ac81709c6fce0cab08b
PatrickBoynton <jpboynton3@gmail.com>
2021-01-13 15:55:19 -0500

    Refactored code.

--- scripts/main.js
+++ scripts/main.js
@@ -2,28 +2,23 @@ const display = document.querySelector(".clock-display");
 const progress = document.querySelector(".clock-progress-bar");
 const face = document.querySelector(".clock-face");
 
-
-// Found this nifty method on SO.
-function checkTime(n) {
-    if (n < 10) {
-        n = "0" + 1;
-    }
-    return n;
-}
+const checkTime = timeInterval => (timeInterval < 10) ? ("0" + timeInterval) : timeInterval;
 
 function getCurrentTime() {
     let date = new Date();
     let hours = date.getHours();
     let minutes = date.getMinutes();
     let seconds = date.getSeconds();
+    let time = `${checkTime(hours)}:${checkTime(minutes)}:${checkTime(seconds)}`
+    let hexCode = `#${checkTime(hours)}${checkTime(minutes)}${checkTime(seconds)}`;
 
-    hours = checkTime(hours);
-    minutes = checkTime(minutes);
-    seconds = checkTime(seconds);
-    display.innerHTML = `${hours}:${minutes}:${seconds}`;
+    display.innerHTML = time;
+    face.style.backgroundColor = hexCode;
 
-    console.log(`#${hours}${minutes}${seconds}`);
-    // console.log(`${hours}:${minutes}:${seconds}`);
+    console.log(time);
+    console.log(hexCode);
 }
+
+getCurrentTime();
 
 setInterval(getCurrentTime, 1000);
