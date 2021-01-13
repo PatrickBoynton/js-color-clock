2622878009a9ac35dbaddad43dc1edd4a7ccc2b9
PatrickBoynton <jpboynton3@gmail.com>
2021-01-13 12:53:59 -0500

    Added a date object and logged it.

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,6 @@ const display = document.querySelector(".clock-display");
 const progress = document.querySelector(".clock-progress-bar");
 const face = document.querySelector(".clock-face");
 
-console.log(display);
-console.log(progress);
-console.log(face);
+const date = new Date();
+
+console.log(date);
