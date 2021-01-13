3990feb18d653b02eecfa03e38b06ffbae5cc50c
PatrickBoynton <jpboynton3@gmail.com>
2021-01-13 12:51:34 -0500

    Added selectors for the elements, and logged them.

---
+++ scripts/main.js
@@ -0,0 +1,7 @@
+const display = document.querySelector(".clock-display");
+const progress = document.querySelector(".clock-progress-bar");
+const face = document.querySelector(".clock-face");
+
+console.log(display);
+console.log(progress);
+console.log(face);
