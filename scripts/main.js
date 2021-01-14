ad7a7aa355df90b6562d1e8233e85d244d33395b
PatrickBoynton <jpboynton3@gmail.com>
2021-01-13 21:51:10 -0500

    Added a counter for the progress bar.

--- scripts/main.js
+++ scripts/main.js
@@ -3,6 +3,7 @@ const progress = document.querySelector(".clock-progress-bar");
 const face = document.querySelector(".clock-face");
 
 const checkTime = timeInterval => (timeInterval < 10) ? ("0" + timeInterval) : timeInterval;
+
 let date;
 let hours;
 let minutes;
@@ -16,15 +17,22 @@ function getCurrentTime() {
     hours = date.getHours();
     minutes = date.getMinutes();
     seconds = date.getSeconds();
+    // I played around with numbers till I got something close.
+    let count = seconds * .22;
 
     time = `${checkTime(hours)}:${checkTime(minutes)}:${checkTime(seconds)}`;
-    hexCode = `#${checkTime(hours)}${checkTime(minutes)}${checkTime(seconds)}`;
+    // Turns hours minutes and seconds all into one big hex code.
+    hexCode = `#${(hours).toString(16)}${minutes.toString(16)}${seconds.toString(16)}`;
 
     display.innerHTML = time;
     face.style.backgroundColor = hexCode;
+
+    // Increments count.
+    count++;
+    progress.style.width = `${count}rem`;
 }
 
-display.addEventListener("mouseover", () => {
+display.addEventListener("mouseover", (e) => {
     display.innerHTML = hexCode;
 });
 
