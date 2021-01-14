c081a4d9487e11384c4619cff9dd1256e5f0060e
PatrickBoynton <jpboynton3@gmail.com>
2021-01-14 09:58:21 -0500

    Did an in class refactor. Added the if else check, and mouseout.

--- scripts/main.js
+++ scripts/main.js
@@ -11,6 +11,7 @@ let seconds;
 
 let time;
 let hexCode;
+let isHovering = false;
 
 function getCurrentTime() {
     date = new Date();
@@ -20,9 +21,14 @@ function getCurrentTime() {
     // I played around with numbers till I got something close.
     let count = seconds * .22;
 
-    time = `${checkTime(hours)}:${checkTime(minutes)}:${checkTime(seconds)}`;
-    // Turns hours minutes and seconds all into one big hex code.
-    hexCode = `#${(hours).toString(16)}${minutes.toString(16)}${seconds.toString(16)}`;
+    // In class refactor.
+    if (isHovering) {
+        // Turns hours minutes and seconds all into one big hex code.
+        time = `#${(hours).toString(16)}${minutes.toString(16)}${seconds.toString(16)}`;
+    } else {
+        time = `${checkTime(hours)}:${checkTime(minutes)}:${checkTime(seconds)}`;
+    }
+
 
     display.innerHTML = time;
     face.style.backgroundColor = hexCode;
@@ -31,9 +37,14 @@ function getCurrentTime() {
     count++;
     progress.style.width = `${count}rem`;
 }
+//refactored in class, changes isHovering based on position.
+display.addEventListener("mouseover", () => {
+    isHovering = true;
 
-display.addEventListener("mouseover", (e) => {
-    display.innerHTML = hexCode;
+});
+//refactored in class, changes isHovering based on position.
+display.addEventListener("mouseout", () => {
+   isHovering = false;
 });
 
 getCurrentTime();
